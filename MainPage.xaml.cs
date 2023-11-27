8a473cc2e59a7c16dac4a526302110e27246330e
vuddanti-15 <vabhinav@stratapps.com>
2023-11-27 10:56:09 +0530

    windows splash screen done

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,16 +2,34 @@
 {
     public partial class MainPage : ContentPage
     {
-        
+
 
         public MainPage()
         {
             InitializeComponent();
+            ShowSplashScreen();
             MyWebView.Source = new Uri("https://xamplify.com/");
         }
 
-       
-        
-    }
+        private async void ShowSplashScreen()
+        {
+            // Simulate a delay to show the splash screen for a few seconds
+            await Task.Delay(5000); // Adjust the duration as needed
 
+
+            // Set the ContentView to an Image displaying the splash screen
+            Content = new Image { Source = "splashwindows.png", Aspect = Aspect.AspectFill };
+
+            // Initialize the WebView
+            MyWebView = new WebView
+            {
+                Source = new Uri("https://xamplify.com/")
+            };
+
+            // Set the ContentView to the WebView
+            Content = MyWebView;
+        }
+    }
 }
+
+
