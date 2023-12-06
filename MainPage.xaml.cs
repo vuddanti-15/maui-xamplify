d73c4d5cd14ec77863846b973ee3514af3b94d27
vuddanti-15 <vabhinav@stratapps.com>
2023-12-06 14:26:01 +0530

    Windows spalsh screen and full screen done

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,9 +1,8 @@
-﻿namespace xamplify
+﻿
+namespace xamplify
 {
     public partial class MainPage : ContentPage
     {
-
-
         public MainPage()
         {
             InitializeComponent();
@@ -12,12 +11,13 @@
 
         private async void ShowSplashScreen()
         {
-            // Simulate a delay to show the splash screen for a few seconds
-            await Task.Delay(5000); // Adjust the duration as needed
-
-
-            // Set the ContentView to an Image displaying the splash screen
-            Content = new Image { Source = "splashwindows.png", Aspect = Aspect.AspectFill };
+            // Check if the current platform is Windows
+            if (Device.RuntimePlatform == Device.WinUI)
+            {
+                // Load the splash screen for Windows
+                Content = new Image { Source = "splashwindows.png", Aspect = Aspect.AspectFill };
+                await Task.Delay(5000); // Adjust the duration as needed
+            }
 
             // Initialize the WebView
             MyWebView = new WebView
@@ -28,7 +28,13 @@
             // Set the ContentView to the WebView
             Content = MyWebView;
         }
+        private void OnCloseButtonClick(object sender, EventArgs e)
+        {
+            // Close the app
+            // Note: The behavior may vary depending on the platform
+            // On some platforms, you may need to use specific APIs to close the app
+            // For simplicity, you can use the following line, but check the platform-specific behavior
+            App.Current.MainPage?.Navigation.PopModalAsync();
+        }
     }
 }
-
-
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
 
 namespace xamplify
 {
@@ -16,7 +17,31 @@ namespace xamplify
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
+#endif
+
+#if WINDOWS
+            builder.ConfigureLifecycleEvents(events =>
+            {
+                // Make sure to add "using Microsoft.Maui.LifecycleEvents;" at the top of the file 
+                events.AddWindows(windowsLifecycleBuilder =>
+                {
+                    windowsLifecycleBuilder.OnWindowCreated(window =>
+                    {
+                        window.ExtendsContentIntoTitleBar = false;
+                        var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                        var id = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(handle);
+                        var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(id);
+                        switch (appWindow.Presenter)
+                        {
+                            case Microsoft.UI.Windowing.OverlappedPresenter overlappedPresenter:
+                                overlappedPresenter.SetBorderAndTitleBar(false, false);
+                                overlappedPresenter.Maximize();
+                                break;
+                        }
+                    });
+                });
+            });
 #endif
 
             return builder.Build();
